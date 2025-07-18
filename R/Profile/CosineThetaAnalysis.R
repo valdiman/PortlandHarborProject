9162d1681afacbae8fe668a758c47a4843bccbf9
Martinez Araneda <martie@uiowa.edu>
2025-07-18 11:51:29 -0500

    still working on it.

--- R/Profile/CosineThetaAnalysis.R
+++ R/Profile/CosineThetaAnalysis.R
@@ -3,10 +3,14 @@
 
 # Packages and libraries needed --------------------------------------------
 # Install packages
+install.packages("tidyr")
+install.packages("dplyr")
 install.packages("lsa")
 install.packages("SnowballC")
 
 # Libraries
+library(tidyr)
+library(dplyr)
 library(lsa)
 
 # Read data ---------------------------------------------------------------
@@ -22,6 +26,45 @@ prof.water <- read.csv("Output/Data/csv/AveProf3HWater.csv", header = TRUE,
 # Flux profile
 prof.flux <- read.csv("Output/Data/csv/PCBFlux.csv", header = TRUE,
                        check.names = FALSE)
+
+# Format data
+air_profile <- prof.air[, grep("^PCB", names(prof.air))]
+air_profile$source <- "air"
+
+aroclor_profile <- prof.aroclor[, grep("^PCB", names(prof.aroclor))]
+aroclor_profile$source <- "aroclor"
+
+water_profile <- prof.water %>%
+  select(congener, mean) %>%
+  pivot_wider(names_from = congener, values_from = mean) %>%
+  mutate(source = "water")
+
+flux_profile <- prof.flux %>%
+  select(Congener, `Mean (ng/m2/d)`) %>%
+  rename(mean = `Mean (ng/m2/d)`) %>%
+  pivot_wider(names_from = Congener, values_from = mean) %>%
+  mutate(source = "flux")
+# Need to change + to .
+names(flux_profile) <- gsub("\\+", ".", names(flux_profile))
+
+# Get column names (excluding the "source" column)
+water_pcbs <- setdiff(names(water_profile), "source")
+flux_pcbs  <- setdiff(names(flux_profile), "source")
+
+# Check if they are the same
+setequal(water_pcbs, flux_pcbs) # True
+
+air_pcbs     <- setdiff(names(air_profile), "source")
+aroclor_pcbs <- setdiff(names(aroclor_profile), "source")
+
+# Check if they are the same
+setequal(air_pcbs, aroclor_pcbs) # True
+
+extra_pcbs <- setdiff(air_pcbs, flux_pcbs)
+
+
+
+
 
 # Format air and aroclors data
 # Removing metadata
