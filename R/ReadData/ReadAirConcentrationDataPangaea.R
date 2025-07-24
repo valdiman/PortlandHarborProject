b743126044ca5bed411c724b10ce7b7d3ccc985a
Martinez Araneda <martie@uiowa.edu>
2025-07-24 15:37:03 -0500

    Initial version.

---
+++ R/ReadData/ReadAirConcentrationDataPangaea.R
@@ -0,0 +1,37 @@
+# R code to download and format data from Pangaea
+
+# Packages and libraries needed -------------------------------------------------------------------
+# Install packages
+install.packages('pangaear')
+# Library
+library(pangaear) # Read data from Pangaea
+
+# Read data from Pangaea and format data ------------------------------
+
+# Read data from Pangaea repository
+# Citation:
+# Slade, Alexis; Martinez, Andres; Mathieu-Campbell, Martine;
+# Watkins, Shannon; Cohen, Cassie; Hornbuckle, Keri C (2025):
+# Airborne polychlorinated biphenyl congener concentrations using
+# PUF-PAS from the Portland Harbor Superfund Site, Portland, OR, 2022
+# [dataset publication series]. [dataset].  PANGAEA, https://doi.pangaea.de/10.1594/PANGAEA.983837
+# final doi: https://doi.org/10.1594/PANGAEA.983837
+
+# Set cache path to the project folder
+pg_cache$cache_path_set(full_path = "Data/")
+# Download original datasets from Pangaea
+d.0 <- pg_data(doi = '10.1594/PANGAEA.983837')
+
+# Extract dataset
+# 4 corresponds to airborne concentration
+d <- d.0[[4]]$data
+
+# Mdify the names of the congeners
+names(d) <- sub(".*\\((.*)\\).*", "\\1", names(d))
+
+# Export modified dataset
+write.csv(d, file = "Data/AirConcV2.csv", row.names = FALSE)
+
+
+
+
